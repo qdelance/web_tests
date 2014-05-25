531ac6c9f01eb7a92de398db7aca80609e09a0ee
qdelance <quentin.delance@gmail.com>
2014-05-25 08:13:26 +0200

    Add geoloc test

---
+++ 92_test_geoloc_html5/geoloc.js
@@ -0,0 +1,20 @@
+function getLocation()
+{
+  if (navigator.geolocation)
+  {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  }
+  else
+  {
+    var x = document.getElementById("demo");
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position)
+{
+  var x = document.getElementById("demo");
+  x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+}
+
+getLocation();
